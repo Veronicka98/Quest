f3ff5ca18fc5be0c5d82b6cb2961e686f218ab14
Veronika Karpenko <Veronicka98@mail.ru>
2018-10-05 09:33:24 +0100

    Initial commit

---
+++ app/src/main/java/org/wit/quest/QuestActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.quest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_quest)
+  }
+}
