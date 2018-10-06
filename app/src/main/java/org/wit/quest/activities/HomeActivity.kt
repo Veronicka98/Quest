47088f01760e197d49b8f70b1c5cf5c35c6e8ca2
Veronika Karpenko <Veronicka98@mail.ru>
2018-10-06 11:12:34 +0100

    Add menu to activities, change xml

--- app/src/main/java/org/wit/quest/activities/HomeActivity.kt
+++ app/src/main/java/org/wit/quest/activities/HomeActivity.kt
@@ -4,39 +4,35 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_quest.*
+import kotlinx.android.synthetic.main.activity_home.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.startActivityForResult
 import org.wit.quest.R
 import org.wit.quest.main.MainApp
 
-class QuestActivity : AppCompatActivity(), AnkoLogger {
+class HomeActivity : AppCompatActivity(), AnkoLogger {
 
-  lateinit var app : MainApp
+  lateinit var app: MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_quest)
+    setContentView(R.layout.activity_home)
 
-    setSupportActionBar(toolbarCreate)
+    setSupportActionBar(toolbarHome)
 
     app = application as MainApp
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.menu_create, menu)
+    menuInflater.inflate(R.menu.menu_main, menu)
     return super.onCreateOptionsMenu(menu)
   }
 
   override fun onOptionsItemSelected(item: MenuItem?): Boolean {
     when (item?.itemId) {
-      R.id.item_cancel -> {
-        setResult(RESULT_CANCELED)
-        finish()
-      }
-      R.id.item_save -> {
-        setResult(200)
-        finish()
-      }
+      R.id.item_add -> startActivityForResult<QuestActivity>(200)
+      R.id.item_list -> startActivityForResult<ListActivity>(200)
+      R.id.item_setting -> startActivityForResult<SettingsActivity>(200)
     }
     return super.onOptionsItemSelected(item)
   }
